8ac8e25bb6a6a1d05a2b8c7d62dc28fdba3ecdd2
Faizan-313 <faizansyedpeer@gmail.com>
2026-04-11 01:20:26 +0530

    add socket setup

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,22 @@
 import express from "express"
 import "dotenv/config"
+import http from "http"
+import { Server } from "socket.io";
+import cors from "cors"
 
-const app = express();
 const port = process.env.PORT || 3000;
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    }
+});
+
+app.use(cors())
+app.use(express.static('public'))
 
 
 app.listen(port, () => {
