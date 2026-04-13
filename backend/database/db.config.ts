4ee5f00825020dcae878fe6a694a368ec1b206d2
Faizan-313 <faizansyedpeer@gmail.com>
2026-04-14 00:02:40 +0530

    fasdf

--- backend/database/db.config.ts
+++ backend/database/db.config.ts
@@ -1,11 +1,11 @@
 import { Pool } from 'pg'
 
 const dbPool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'find-spy',
-    password: 'database@sql',
-    port: 5432,
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'find-spy',
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
 })
 
 export default dbPool;
