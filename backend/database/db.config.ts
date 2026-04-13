d937e4a1d81aebedc23b6c6658f9b42b4d4009a2
Faizan-313 <faizansyedpeer@gmail.com>
2026-04-14 01:00:14 +0530

    asf

--- backend/database/db.config.ts
+++ backend/database/db.config.ts
@@ -1,11 +1,19 @@
 import { Pool } from 'pg'
 
 const dbPool = new Pool({
-    user: process.env.DB_USER || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'find-spy',
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+})
+
+dbPool.on('connect', () =>{
+    console.log("Connection pool established with database")
+})
+
+dbPool.on('error', (err) => {
+    console.error("Count not establish connection with database: ", err);
 })
 
 export default dbPool;
